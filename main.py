65a01de9d25ae666a5524745c0cfe68372ec5fbb
Sambidh <raisambidh@gmail.com>
2024-05-12 19:46:11 +0545

    created channel id finder.py and updated the codes of other files

--- main.py
+++
@@ -1,9 +0,0 @@
-import pytube
-import urllib.request
-
-def download_thumbnail(url):
-    video = pytube.YouTube(url)
-    thumbnail_url = video.thumbnail_url
-    urllib.request.urlretrieve(thumbnail_url, "thumbnails/download.jpg")
-
-download_thumbnail("https://www.youtube.com/watch?v=QHaKaeb7vfI")
